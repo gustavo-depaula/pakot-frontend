4267a065420e8a5c56aaba6c4821ccdfa749f171
Gustavo de Paula <gdpaula2000@gmail.com>
2017-08-06 21:14:31 -0300

    Autenticação Pt2
    Login partido em duas partes, fb auth e seleção se quer entrar na conta de user ou entregador

--- src/components/login.vue
+++ src/components/login.vue
@@ -21,7 +21,7 @@
 							<img src="../assets/package.png" alt="Pakot logo" width="150px">
 						</div>
 					</div>
-					<div class="hero-body">
+					<div v-if="user.object != null" class="hero-body">
 						<div class="container">
 							<div class="columns">
 								<div class="column is-8 is-offset-2">
@@ -29,14 +29,35 @@
 										<h3 class="title">Cliente</h3>
 										<h2 class="subtitle">Seja independente dos correios.</h2>
 										<p class="control login">
-											<button @click="signIn" class="button is-info is-large is-fullwidth" :class="{ 'is-loading': btnLoading, 'is-outlined': !btnLoading }"><span class="icon"><i class="fa fa-facebook"></i></span></button>
+											<button @click="signUser" class="button is-success is-large is-fullwidth" :class="{ 'is-loading': userBtnLoading, 'is-outlined': !userBtnLoading }"><span class="icon"><i class="fa fa-arrow-right"></i></span></button>
 										</p>
 										<hr>
 										<h3 class="title">Entregador</h3>
 										<h2 class="subtitle">Ajude a carregar a disrupção.</h2>
 										<p class="control login">
-											<button class="button is-info is-outlined is-large is-fullwidth"><span class="icon"><i class="fa fa-facebook"></i></span></button>
+											<button @click="signDeliveryMan" class="button is-success is-large is-fullwidth" :class="{ 'is-loading': deliveryBtnLoading, 'is-outlined': !deliveryBtnLoading }"><span class="icon"><i class="fa fa-arrow-right"></i></span></button>
 										</p>
+									</div>
+									<div class="section copyheart">
+										<p class="has-text-centered">
+											<span>&hearts;&nbsp;2017 Pakot Technologies Inc.</span>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div v-else class="hero-body">
+						<div class="container">
+							<div class="columns">
+								<div class="column is-8 is-offset-2">
+									<div class="login-form">
+										<h3 class="title">Entre agora</h3>
+										<h2 class="subtitle">Faça parte do Pakot.</h2>
+										<p class="control login">
+											<button @click="signIn" class="button is-info is-large is-fullwidth" :class="{ 'is-loading': authBtnLoading, 'is-outlined': !authBtnLoading }"><span class="icon"><i class="fa fa-facebook"></i></span></button>
+										</p>
+										<hr>
 									</div>
 									<div class="section copyheart">
 										<p class="has-text-centered">
@@ -55,51 +76,87 @@
 </template>
 <script>
 	import router from '../router'
+	import axios from 'axios'
 
 	export default {
 		name: 'Login',
 		data (){
 			return {
-				btnLoading: false,
-				requireSignUp: this.$store.getters.requireSignUp,
+				authBtnLoading: false,		
+				userBtnLoading: false,
+				deliveryBtnLoading: false,
+				user: this.$store.getters.user,
 			}
 		},
 		methods: {
+			signUserButtonHandling (){
+				// signUser()
+			},
+			signUser (){
+				this.userBtnLoading = true
+				this.$store.commit('isUser')
+				var user = this.$store.getters.user
+				axios.post('https://pakot-backend.herokuapp.com/public/login/User', {email: user.object.email})
+					.then(response => {
+						console.log(response.data)
+						if (response.data == 'requireSignUp') {
+							this.$store.commit('userLogIn', user.object)
+							this.$store.commit('userRequireSignUp')
+						} else {
+							this.$store.commit('userLogIn', user.object)
+							this.$store.commit('userDontRequireSignUp')
+							axios.post('https://pakot-backend.herokuapp.com/public/User/getData', {email: this.$store.getters.user.object.email})
+							.then(response => {
+								console.log(response)
+								this.$store.commit('cpf', response.data.cpf) 
+								this.$store.commit('phone', response.data.phone)
+							})
+							this.$router.push('/packages')
+						}
+					})
+					// .catch(e => {
+					// 	console.log(e)
+					// })
+			},
+			signDeliveryMan (){
+				this.deliveryBtnLoading = true
+				this.$store.commit('isDeliveryMan')
+				var user = this.$store.getters.user
+				axios.post('https://pakot-backend.herokuapp.com/public/login/DeliveryMan', {email: user.object.email})
+					.then(response => {
+						console.log(response.data)
+						if (response.data == 'requireSignUp') {
+							this.$store.commit('userLogIn', user.object)
+							this.$store.commit('userRequireSignUp')
+						} else {
+							this.$store.commit('userLogIn', user.object)
+							this.$store.commit('userDontRequireSignUp')
+							axios.post('https://pakot-backend.herokuapp.com/public/User/getData', {email: this.$store.getters.user.object.email})
+							.then(response => {
+								console.log(response)
+								this.$store.commit('cpf', response.data.cpf) 
+								this.$store.commit('phone', response.data.phone)
+							})
+							this.$router.push('/packages')
+						}
+					})
+					// .catch(e => {
+					// 	console.log(e)
+					// })
+			},
 			signIn (){
-				this.btnLoading = true
-				var user
-				firebase.auth().signInWithRedirect(new firebase.auth.FacebookAuthProvider()).then(function(result) {
-				// This gives you a Facebook Access Token. You can use it to access the Facebook API.
-				var token = result.credential.accessToken;
-				// The signed-in user info.
-				// console.log('acerto')
-				user = result.user;
-				// console.log()
-				// ...
-				// localStorage.setItem('user', 's')
-			}).catch(function(error) {
-				// Handle Errors here.
-				console.log(error)
-				var errorCode = error.code;
-				var errorMessage = error.message;
-				// The email of the user's account used.
-				var email = error.email;
-				// The firebase.auth.AuthCredential type that was used.
-				var credential = error.credential;
-				// ...
-			})
+				this.authBtnLoading = true
+				firebase.auth().signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+			}
 		},
-
-		toTrue (){
-			this.$store.commit('userLogIn', {})
-		}
-	},
-	mounted (){
+		computed: {
+		},
+		mounted (){
 		// auth.signIn()
 		// this.$store.state.user.logged = true
 
+		}
 	}
-}
 </script>
 
 <style scoped src="../css/login.css"/>
