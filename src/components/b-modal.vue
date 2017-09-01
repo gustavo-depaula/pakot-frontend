d7d096a6c34a3e995e96575bc5e98a9bab2f6d91
Gustavo de Paula <gdpaula2000@gmail.com>
2017-09-01 18:27:11 -0300

    emits no modal

--- src/components/b-modal.vue
+++ src/components/b-modal.vue
@@ -36,9 +36,9 @@
 				</p>
 				<div id="mapModal"></div>
 				<div v-if="isDeliveryMan">
-					<button v-if="state == 0" @click="$emit('accept')" class="actionBtn button is-warning is-medium"><span class="icon"><i class="fa fa-check"></i></span>&nbsp;&nbsp;{{btnMessage}}</button>
-					<button v-if="state == 1" @click="$emit('accept')" class="actionBtn button is-warning is-medium"><span class="icon"><i class="fa fa-check"></i></span>&nbsp;&nbsp;{{btnMessage}}</button>
-					<button v-if="state == 2" @click="$emit('accept')" class="actionBtn button is-warning is-medium"><span class="icon"><i class="fa fa-check"></i></span>&nbsp;&nbsp;{{btnMessage}}</button>
+					<button v-if="state == 0" class="actionBtn button is-warning is-medium"><span class="icon"><i class="fa fa-check"></i></span>&nbsp;&nbsp;{{btnMessage}}</button>
+					<button v-if="state == 1" @click="$emit('arrived')" class="actionBtn button is-warning is-medium"><span class="icon"><i class="fa fa-check"></i></span>&nbsp;&nbsp;{{btnMessage}}</button>
+					<button v-if="state == 2" @click="$emit('dispatch')" class="actionBtn button is-warning is-medium"><span class="icon"><i class="fa fa-check"></i></span>&nbsp;&nbsp;{{btnMessage}}</button>
 					<button v-if="state == 3" @click="$emit('accept')" class="actionBtn button is-warning is-medium"><span class="icon"><i class="fa fa-check"></i></span>&nbsp;&nbsp;{{btnMessage}}</button>
 					
 				</div>
